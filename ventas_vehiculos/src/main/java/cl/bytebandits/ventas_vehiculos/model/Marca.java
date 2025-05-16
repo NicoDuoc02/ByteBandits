c6e7d0077b9723340d9e3631f5502ea87914447f
AcuestateGoku <gabr.hermosilla@duocuc.cl>
2025-05-16 19:47:33 -0400

    commit

--- ventas_vehiculos/src/main/java/cl/bytebandits/ventas_vehiculos/model/Marca.java
+++ ventas_vehiculos/src/main/java/cl/bytebandits/ventas_vehiculos/model/Marca.java
@@ -1,5 +1,34 @@
 package cl.bytebandits.ventas_vehiculos.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+//indica que es una entidad en JPA
+@Entity
+
+//indica la tabla en la base de datos
+@Table(name="MARCA")
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Marca {
+
+//Especifica que es la PK
+@Id
+//nombre real de la columna
+@Column(name="ID_MARCA")
+private Integer idMarca;
+@Column(name="NOMBRE_MARCA")
+private String nombreMarca;
+@Column(name="ID_PAIS_ORIGEN")
+private PaisOrigen paisOrigen;
 
 }
--- ventas_vehiculos/src/main/java/cl/bytebandits/ventas_vehiculos/model/Modelo.java
+++ ventas_vehiculos/src/main/java/cl/bytebandits/ventas_vehiculos/model/Modelo.java
@@ -1,5 +1,33 @@
 package cl.bytebandits.ventas_vehiculos.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+//indica que es una entidad en JPA
+@Entity
+
+//indica la tabla en la base de datos
+@Table(name="MARCA")
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Modelo {
+//Especifica que es la PK
+@Id
+//nombre real de la columna
+@Column(name="ID_MODELO")
+private Integer idModelo;
+@Column(name="NOMBRE_MODELO")
+private String nombreMarca;
+@Column(name="ID_TIPO_VEHICULO")
+private TipoVehiculo tipoVehiculo;
 
 }
