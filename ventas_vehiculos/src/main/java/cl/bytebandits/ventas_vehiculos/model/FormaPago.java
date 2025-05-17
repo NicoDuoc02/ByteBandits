a1871f3b54a15a62897eeb2260349e5d26db5dc3
AcuestateGoku <gabr.hermosilla@duocuc.cl>
2025-05-16 20:09:45 -0400

    commit

--- ventas_vehiculos/src/main/java/cl/bytebandits/ventas_vehiculos/model/FormaPago.java
+++ ventas_vehiculos/src/main/java/cl/bytebandits/ventas_vehiculos/model/FormaPago.java
@@ -1,5 +1,32 @@
 package cl.bytebandits.ventas_vehiculos.model;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+//indica que es una entidad en JPA
+@Entity
+
+//indica la tabla en la base de datos
+@Table(name="FORMA_PAGO")
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 
 public class FormaPago {
+    
+//Especifica que es la PK
+@Id
+//nombre real de la columna
 
+@Column(name="ID_FORMA_PAGO")
+private Integer idFormaPago;
+@Column(name="NOMBRE_FORMA_PAGO")
+private String nombreFormaPago;
 }
