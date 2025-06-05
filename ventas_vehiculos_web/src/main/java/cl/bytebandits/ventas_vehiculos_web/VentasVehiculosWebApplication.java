b2159623a07ca066c62c6153ad2cd490bd663d10
NicoDuoc02 <169114151+NicoDuoc02@users.noreply.github.com>
2025-06-05 18:06:15 -0400

    login hecho
    pero falta crear un regiistrar

---
+++ ventas_vehiculos_web/src/main/java/cl/bytebandits/ventas_vehiculos_web/VentasVehiculosWebApplication.java
@@ -0,0 +1,15 @@
+package cl.bytebandits.ventas_vehiculos_web;
+
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VentasVehiculosWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VentasVehiculosWebApplication.class, args);
+	}
+
+}
--- ventas_vehiculos_web/src/test/java/cl/bytebandits/ventas_vehiculos_web/VentasVehiculosWebApplicationTests.java
+++ ventas_vehiculos_web/src/test/java/cl/bytebandits/ventas_vehiculos_web/VentasVehiculosWebApplicationTests.java
@@ -1,10 +1,10 @@
-package cl.bytebandits.venta_autos_web;
+package cl.bytebandits.ventas_vehiculos_web;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class VentaAutosWebApplicationTests {
+class VentasVehiculosWebApplicationTests {
 
 	@Test
 	void contextLoads() {
